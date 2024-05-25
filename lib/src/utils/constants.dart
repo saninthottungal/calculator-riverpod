36425ceb681f38c14666b50e1d05ac6cad8eb04f
sanin <saninthottungalhere@gmail.con>
2024-05-25 18:20:37 +0530

    added custom colors

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//constant colors
+
+class AppColors {
+  static const orange = Color.fromARGB(255, 255, 159, 10);
+  static const hash = Color.fromARGB(255, 165, 165, 165);
+  static const grey = Color.fromARGB(255, 165, 165, 165);
+}
