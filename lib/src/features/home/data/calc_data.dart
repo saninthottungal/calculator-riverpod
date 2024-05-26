197ddee7d1ec7f470714ecd90e7ec3f9f13765c3
sanin <saninthottungalhere@gmail.con>
2024-05-26 13:56:48 +0530

    methods created, yet to define

--- lib/src/features/home/data/calc_data.dart
+++ lib/src/features/home/data/calc_data.dart
@@ -8,7 +8,43 @@ class CalcData extends Notifier<String> {
   }
 
   void compute(String value) {
-    switch (value) {}
-    state == Calnum.n0 ? state = value : state = state + value;
+    switch (value) {
+      case Calnum.clear:
+        ref.invalidateSelf();
+        break;
+      case Calnum.delete:
+        state = state.substring(0, state.length - 1);
+        break;
+      case Calnum.percent:
+        //TODO add percent calculation
+        break;
+      case Calnum.div:
+        div();
+        break;
+      case Calnum.mult:
+        mult();
+        break;
+      case Calnum.sub:
+        sub();
+        break;
+      case Calnum.add:
+        addf();
+        break;
+      case Calnum.equal:
+        equate();
+        break;
+      default:
+        state == Calnum.n0 ? state = value : state = state + value;
+    }
   }
+
+  void div() {}
+
+  void mult() {}
+
+  void sub() {}
+
+  void addf() {}
+
+  void equate() {}
 }
